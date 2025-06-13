15ccf430e4d80b688abbd44d71639f7d6053a3af
Raflifauzanrauf <raflifauzanr@gmail.com>
2025-06-13 19:21:04 +0700

    success

--- src/main/java/com/example/currency/App.java
+++ src/main/java/com/example/currency/App.java
@@ -3,7 +3,7 @@ package com.example.currency;
 public class App {
     public static void main(String[] args) {
         Converter converter = new Converter();
-        double result = converter.convert("USD", "IDR", 10));
+        double result = converter.convert("USD", "IDR", 10);
         System.out.println("10 USD in IDR: " + result);
     }
 }
